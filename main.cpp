0a800aaf7d1d428abd31e15f24edf14119dc20d8
matkatmusic <matkatmusic@gmail.com>
2020-02-14 14:18:39 -0800

    added part5 instructions

--- main.cpp
+++ main.cpp
@@ -1,48 +1,60 @@
-/*
-Project 4: Part 4 / 9
- Chapter 4 Part 7
- Function/Constructor Overloading
 
- Create a branch named Part4
+#include <iostream>
+/*
+Project 4: Part 5 / 9
+ video: Chapter 5 Part 2
+ Operator Overloading.
+
+ Create a branch named Part5
 
     Build/Run often with this task to make sure you're not breaking the code with each step.
     I recommend committing after you get each step working so you can revert to a working version easily if needed.
  
- 1) add pow() functions, and a powInternal() function to each of your UDTs
-     a) your pow() functions should call powInternal()
-     b) add a pow() whose argument type is the primitive your UDT owns.  the argument should be passed by copy.
-     c) for each UDT in the file, your class should have pow() overloads that take that UDT as the function argument.
-         the argument should be passed as const ref
-         i.e. if you had UDTs named IntType, FloatType, DoubleType
-             in your IntType class, you'd have:
-                 pow(const IntType&),
-                 pow(const FloatType&),
-                 pow(const DoubleType&),
-                 and pow(int)
-     d) be sure to remember the rules about what member functions can be called on const objects.
-             (const objects can only call their const member functions)
+ 1) replace the add/subtract/multiply/etc functions with overloaded math operators 
+        e.g. add() would become operator+=() because it modifies the internal heap-allocated object.
  
- 2) your powInternal() function should do something like this in its body:    *val = std::pow( *val, arg );
-         where 'arg' is the passed-in type, converted to whatever type your object is holding.
-             if your UDT owns an int, then arg would be an int.
-             if your UDT owns a float, then arg would be a float.
-         std::pow's documentation is found here: https://en.cppreference.com/w/cpp/numeric/math/pow so be sure to include
-             the proper header file listed there.
+ 2) Your overloaded operators should only take primitives, passed by value.
  
- 3) modify the Point class below to have Constructors that accept your UDTs.
-     a) make the Constructor's UDT arguments initialize the Point class's two member variables.
-     b) overload the multiply() function so it can accept each of your UDTs.  I've added an implementation you can mimick for this function.
-     c) add a toString() function to the Point class that prints out the x and y members via std::cout.
+ 3) don't delete your conversion functions.
+         if you removed them in Chapter 4 Part 7, put them back in.
+         uncomment them if you commented them in Chapter 4 Part 7
  
- 4) clear out your old main()
-     a) use your new pow() function in main for all of your types and print out the results.
-     b) use the Point class in main and multiply some Point instances with your UDTs
-     c) use the Point::toString() function to print out the results.
- 
- 
- 5) make sure it compiles without errors.
- 
- You will need to use Forward Declaration and out-of-class definitions to complete this.
+ 4) your main() function should be the same as Chapter 4 Part 7
+     
+ 5) if your Chapter4 Part7 task didn't use any [+ - * or /], 
+         add some in and fix the build errors that might result via the techniques you have learned in the previous weeks (hint: casting)
+         i.e.
+ */
+#if false
+namespace Example
+{
+    int main()
+    {
+        FloatType floatNum(4.3f);
+        IntType intNum(2);
+        IntType intNum2(6);
+
+        /* 
+        if you previously had a line like this demonstrating chaining:
+            
+            intNum.add(3).add(4.5f).divide(floatNum); 
+
+        it should become:
+        */
+        intNum += 3;
+        intNum += 4.5f;
+        intNum /= floatNum;
+        std::cout << "intNum: " << intNum << std::endl;
+
+        intNum = 2 + (intNum2 - 4) + floatNum / 2.3;
+        std::cout << "intNum: " << intNum << std::endl;
+        
+        return 0;
+    }
+}
+#endif
+ /*
+ 6) compile/link/run to make sure you don't have any errors
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
@@ -357,6 +369,7 @@ IntType& IntType::pow(const DoubleType& myNumber)
 
 int main()
 {
+    Example::main();
     // test pow
     IntType it1(4);
     FloatType ft1(2.25f);
