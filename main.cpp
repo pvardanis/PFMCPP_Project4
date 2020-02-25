2eaa40ec7881333e7a835a37e950124a57a2bd8a
matkatmusic <matkatmusic@gmail.com>
2020-02-24 21:58:23 -0800

    Added part8 instructions

--- main.cpp
+++ main.cpp
@@ -1,64 +1,93 @@
 /*
-Project 4: Part 7 / 9
-Video: Chapter 5 Part 4
+Prject 4: Part 8 / 9
+ video: Chapter 5 Part 6 Task 
 
-Create a branch named Part7
+Create a branch named Part8
 
-Templates and Containers
+ R-Values L-Values Move Semantics.
+ 
+ replace main() with the main at the bottom of this file.
+
+ This Task is going to demonstrate R-Values and writing Generic Code
 
     Build/Run often with this task to make sure you're not breaking the code with each step.
     I recommend committing after you get each step working so you can revert to a working version easily if needed.
     it's very easy to mess this task up. 
-
-#1) if you're not using std::unique_ptr to own your heap-allocated type as a member variable, 
-    replace your manual memory management techniques with a private std::unique_ptr member variable.
-
-#2) replace your Heap-Allocated Numeric Type-owning class with a templated class called Numeric.
-        replace all of your previous instances of your separate classes with this templated class.
-
-#3) add a 'using <some name> = <your class template parameter>;' 
-        treat this type declaration via 'using' as a static member variable
-        use this Type alias as the argument everywhere you previously used the template argument.
-        this will make it very easy to change the type if needed.
-            i.e. if you have 'std::unique_ptr<NumericType> value;' before
-                you'd replace NumericType in that variable declaration with '<some name>'
-        
-#4) you'll need to pair it with decltype() to help the compiler figure out the type of the object 
-    your class owns when you make your lambda and free function that takes your unique_ptr.  
-    i.e. like this for determining the template parameter of the unique_ptr function argument to your class's apply() function
-        std::unique_ptr< decltype( <instance of class> )::<some name> >
-    
-#5) template your free function for the apply() that takes a function pointer so it can be used with your Wrapper class's apply() function
-
-#6) add an explicit template specialization for 'double' of your wrapper class
-        this template specialization will have one apply() function instead of 2. 
-
-#7) this apply() function will be templated, and expect a Callable object, not std::function<>.   //need to see if I can do this.
-        the function should allow for chaining.  
-        the callable object should return void, like the function pointer-based apply() function in the primary class template
-
-#8) instantiate your explicit template specialization
-
-#9) call the apply function twice, once with a lambda and once with the free function
-        the free function is templated, so you might need to call it including the template arguments.
-        
-#10) in addition to using the lambda argument to modify the owned object:  (your previous task's lambdas did this same thing) 
-        make the lambda use your explicit template instance (maybe via a std::cout), 
-
-
-
-compile/link/run to make sure you don't have any errors
-
-Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+ Edit your Chapter 5 Part 4 Task and replace its instructions with these instructions
+ 
+ Your job is to replace the owned type (the primitive specified by your template argument) from the 
+     Templated Class you created in Ch5 p04 with a struct named Temporary that can behave ENTIRELY as a temporary object.
+ 
+ That means you must use conversion functions to interact with what it owns.
+ 
+ You need to figure out how to use conversion functions to be able to GET and SET the 'value' member variable.
+    hint: conversion functions can return by value and also by ___...
+  
+ 1) Here is a starting point for how to implement your Temporary struct.
+ */
 
-send me a DM to check your pull request
+#include <typeinfo>
+template<typename NumericType>
+struct Temporary
+{
+    Temporary(NumericType t) : v(t)
+    {
+        std::cout << "I'm a Temporary<" << typeid(v).name() << "> object, #"
+                  << counter++ << std::endl;
+    }
+    /*
+     revise these conversion functions to read/write to 'v' here
+     hint: what qualifier do read-only functions usually have?
+     */
+    operator ___() { /* read-only function */ }
+    operator ___() { /* read/write function */ }
+private:
+    static int counter;
+    NumericType v;
+};
 
- Wait for my code review.
-
-If you need to view an example, see: https://bitbucket.org/MatkatMusic/pfmcpptasks/src/master/Projects/Project4/Part7Example.cpp
+/*
+ 2) add the definition of Temporary::counter here, which is a static variable and must be defined outside of the class.
+    Remember the rules about how to define a Template member variable/function outside of the class.
 */
+
+/*
+ 3) You'll need to template your overloaded math operator functions in your Templated Class from Ch5 p04
+    use static_cast to convert whatever type is passed in to your template's NumericType before performing the +=, -=, etc.  here's an example implementation:
+ */
+namespace example
+{
+template<typename NumericType>
+struct Numeric
+{
+    //snip
+    template<typename OtherType>
+    Numeric& operator-=(const OtherType& o) 
+    { 
+        *value -= static_cast<NumericType>(o); 
+        return *this; 
+    }
+    //snip
+};
+}
+
+/*
+ 4) remove your specialized <double> template of your Numeric<T> class from the previous task (ch5 p04)
+    replace the 2 apply() functions in your Numeric<T> with the single templated apply() function from the specialized <double> template.
+ */
+
+/*
+ 5) Template your pow() function the same way you templated the overloaded math operators
+    Remove the call to powInternal() and just call std::pow() directly.
+    you'll need to static_cast<> the pow() argument the same way you did in the overloaded math operators, when you pass it to std::pow()
+ */
+/*
+ 
+ 6) Finally, your conversion function in your templated class is going to be returning this Temporary, 
+        so you should probably NOT return by copy if you want your templated class's owned object to be modified by any math operation.
+    See the previous hint for implementing the conversion functions for the Temporary if you want to get the held value
+ */
 
 #include <iostream>
 #include <cmath>
@@ -219,7 +248,6 @@ void updateValue(std::unique_ptr<numType>& value)
     *value += 5;
 }
 
-// template<typename numType>
 struct Point
 {
     Point(float _x, float _y): x(_x), y(_y) { } 
@@ -242,151 +270,127 @@ void Point::toString()
     std::cout << "x: " << x << ", y: " << y << std::endl;
 }
 
-int part6main()
-{
-    // test pow
-    Numeric<int> it1(4);
-    Numeric<float> ft1(2.25f);
-    Numeric<double> dt1(5.4);
-
-    Numeric<int> it2(2);
-    Numeric<float> ft2(5.5f);
-    Numeric<double> dt2(1.25);
-
-    Numeric<int> it3(3);
-    Numeric<float> ft3(1.15f);
-    Numeric<double> dt3(3.25); 
-
-    // I put small numbers to prevent overflow
-
-    std::cout << "Test IntType: " << std::endl;
-    std::cout << "4 ^ 2.25f ^ 5.4  = " << it1.pow(static_cast<int>(ft1)).pow(static_cast<int>(dt1)) << std::endl; 
-
-    std::cout << "\nTest FloatType: " << std::endl;
-    std::cout << "5.5f ^ 2 ^ 1.25  = " << ft2.pow(static_cast<float>(it2)).pow(static_cast<float>(dt2)) << std::endl;
-
-    using NumericFt3 = decltype(it3)::myType;
-    using NumericIt3 = decltype(dt3)::myType;
-    // std::cout << "\nTest DoubleType: " << std::endl;
-    std::cout << "3.25 ^ 1.15f ^ 3  = " << dt3.pow(static_cast<NumericFt3>(ft3)).pow(static_cast<NumericIt3>(it3)) << std::endl;
-    std::cout << "\n" << std::endl;
-
-    // test Point
-    Numeric<int> itp(4);
-    Numeric<float> ftp(5.25f);
-    Numeric<double> dtp(2.25);
-
-    Point p1(itp, ftp);
-    std::cout << "p1 values: " << std::endl;
-    p1.toString();
-    p1.multiply(static_cast<float>(dtp)); 
-    std::cout << "\np1 values after multiplication with 2.25: " << std::endl;
-    p1.toString();
-
-    Point p2(itp, static_cast<float>(dtp));
-    std::cout << "\np2 values: " << std::endl;
-    p2.toString();
-    p2.multiply(ftp); 
-    std::cout << "\np2 values after multiplication with 5.25f: " << std::endl;
-    p2.toString();
-
-    Point p3(ftp, static_cast<float>(dtp));
-    std::cout << "\np3 values: " << std::endl;
-    p3.toString();
-    p3.multiply(itp); 
-    std::cout << "\np3 values after multiplication with 4: " << std::endl;
-    p3.toString();
-    
-    // deleted this because it's my new main
-
-    return 0;    
-}
-
+#include <iostream>
 int main()
 {
-    part6main();
-    std::cout << "********************" << std::endl;   
-    Numeric<float> ft(2.5f);
+    Numeric<float> f(0.1f);
+    Numeric<int> i(3);
+    Numeric<double> d(4.2);
     
-    using NumericFt = decltype(ft)::myType;
-
-    std::cout << "FloatType: " << ft << std::endl;
+    f += 2.f;
+    f -= i;
+    f *= d;
+    f /= 2.f;
+    std::cout << "f: " << f << std::endl;
     
-    ft.apply([&](std::unique_ptr<NumericFt>& value) -> decltype(ft)&
-            {
-                *value *= 2;
-                return ft;
-            });
+    d += 2.f;
+    d -= i;
+    d *= f;
+    d /= 2.f;
+    std::cout << "d: " << d << std::endl;
     
-    std::cout << "Multiply by 2 FloatType (lambda): " << ft << std::endl;
-
-    ft.apply(updateValue<float>);
-    std::cout << "updateValue (adds 5): " << ft << std::endl;
-
-    ft += 3; // operation +=
-    ft -= 4.5f; // operation -=
-    ft /= 2; // operation /=
-    ft *= static_cast<float>(2.5); // operation *=
-    std::cout << "+ 3 - 4.5f / 2 * 2.5 = " << ft << std::endl;
-
-    ft.pow(2.5f); // pow
-    std::cout << "pow ^ 2.5f: " << ft << std::endl;
-
-    // double
-    Numeric<double> dt(5.75);
+    i += 2.f; i -= f; i *= d; i /= 2.f;
+    std::cout << "i: "<< i << std::endl;
     
-    std::cout << "********************" << std::endl;
-
-    std::cout << "DoubleType: " << dt << std::endl;
+    Point p(f, i);
+    p.toString();
     
-    dt.apply([&](std::unique_ptr<double>& value) 
-            {
-                *value *= 2;
-                std::cout << "Multiply by 2 DoubleType (lambda): " << *value << std::endl;
-            });
-
-    dt.apply(updateValue<double>); // why do I need to specify <double>? Otherwise it won't work.
-    std::cout << "updateValue (adds 5): " << dt << std::endl;
-
-    dt += 5; // operation +=
-    dt -= 2.5; // operation -=
-    dt /= static_cast<double>(4.5f); // operation /=
-    dt *= 2; // operation *=
-    std::cout << "+ 5 - 2.5 / 4.5f * 2 = " << dt << std::endl;
-
-    dt.pow(4); // pow
-    std::cout << "pow ^ 4: " << dt << std::endl;
-
-    std::cout << "********************" << std::endl;
-
-    // int
-    Numeric<int> it(3);
+    d *= -1;
+    std::cout << "d: " << d << std::endl;
     
-    using NumericIt = decltype(it)::myType;
-
-    std::cout << "IntType: " << it << std::endl;
+    p.multiply(d.pow(f).pow(i));
+    std::cout << "d: " << d << std::endl;
     
-    it.apply([&](std::unique_ptr<NumericIt>& value) -> decltype(it)&
-            {
-                *value *= 4.5f;
-                return it;
-            });
+    p.toString();
     
-    std::cout << "Multiply by 4.5f DoubleType (lambda): " << it << std::endl;
-
-    it.apply(updateValue<int>);
-    std::cout << "updateValueInt (adds 5): " << it << std::endl;
+    Numeric<float> floatNum(4.3f);
+    Numeric<int> intNum(2);
+    Numeric<int> intNum2(6);
+    intNum = 2 + (intNum2 - 4) + floatNum / 2.3;
+    std::cout << "intNum: " << intNum << std::endl;
     
-    it += 5; // operation +=
-    it -= static_cast<NumericIt>(4.25f); // operation -=
-    it /= static_cast<NumericIt>(2.5); // operation /=
-    it *= 3; // operation *=
-    std::cout << "+ 5 - 4.25f / 2.5 * 3 = " << it << std::endl;
-
-    it.pow(static_cast<NumericIt>(3.5));  
-    std::cout << "pow ^ 3.5: " << it << std::endl; 
-
-    std::cout << "good to go!" << std::endl;
-
-    return 0;    
+    {
+        using Type = decltype(f)::Type;
+        f.apply([&f](std::unique_ptr<Type>&value) -> decltype(f)&
+                {
+                    auto& v = *value;
+                    v = v * v;
+                    return f;
+                });
+        std::cout << "f squared: " << f << std::endl;
+        
+        f.apply( cube<Type> );
+        std::cout << "f cubed: " << f << std::endl;
+    }
+    
+    {
+        using Type = decltype(d)::Type;
+        d.apply([&d](std::unique_ptr<Type>&value) -> decltype(d)&
+                {
+                    auto& v = *value;
+                    v = v * v;
+                    return d;
+                });
+        std::cout << "d squared: " << d << std::endl;
+        
+        d.apply( cube<Type> );
+        std::cout << "d cubed: " << d << std::endl;
+    }
+    
+    {
+        using Type = decltype(i)::Type;
+        i.apply([&i](std::unique_ptr<Type>&value) -> decltype(i)&
+                {
+                    auto& v = *value;
+                    v = v * v;
+                    return i;
+                });
+        std::cout << "i squared: " << i << std::endl;
+        
+        i.apply( cube<Type> );
+        std::cout << "i cubed: " << i << std::endl;
+    }
 }
+/*
+ If you did everything correctly, this is the output you should get:
+ 
+I'm a Temporary<f> object, #0
+I'm a Temporary<i> object, #0
+I'm a Temporary<d> object, #0
+f: -1.89
+d: -3.024
+i: -9
+Point { x: -1.89, y: -9 }
+d: 3.024
+I'm a Temporary<d> object, #1
+I'm a Temporary<d> object, #2
+d: 1.49519e+08
+Point { x: -2.82591e+08, y: -1.34567e+09 }
+I'm a Temporary<f> object, #1
+I'm a Temporary<i> object, #1
+I'm a Temporary<i> object, #2
+I'm a Temporary<i> object, #3
+intNum: 5
+I'm a Temporary<f> object, #2
+f squared: 3.5721
+I'm a Temporary<f> object, #3
+f cubed: 45.5796
+I'm a Temporary<d> object, #3
+d squared: 2.2356e+16
+I'm a Temporary<d> object, #4
+d cubed: 1.11733e+49
+I'm a Temporary<i> object, #4
+i squared: 81
+I'm a Temporary<i> object, #5
+i cubed: 531441
+
+Use a service like https://www.diffchecker.com/diff to compare your output. 
+
+Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+
+send me a DM to check your pull request
+
+ Wait for my code review.
+*/
